47446efbffb17657ac96998d409b6d6c7db0f763
Matthias Hampel <matthiashampel@mailbox.org>
2020-03-06 20:05:36 +0100

    remove authorization header

--- src/redux/genres/actions.ts
+++ src/redux/genres/actions.ts
@@ -4,10 +4,7 @@ import { FETCH_GENRES, FetchGenresAction } from './reducers';
 export function fetchGenres() {
   return async function(dispatch: Dispatch<FetchGenresAction>): Promise<Action> {
     const res = await fetch('https://react-cinema.netlify.com/api/genres', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_MOVIE_DB_API_KEY}`
-      }
+      method: 'GET'
     });
 
     const genres = await res.json();
